c82672061fb1c93721c3f68b0a13a584ff173cc7
Sohal Patel <sohal@kodezine.com>
2018-08-29 14:16:20 +0200

    gcov integrated

--- src/prime.c
+++ src/prime.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <errno.h>
 unsigned int checkIsPrime(unsigned int test)
 {
     unsigned int i,rem,isPrime;
@@ -23,25 +24,46 @@ unsigned int checkIsPrime(unsigned int test)
     return(isPrime);
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    unsigned int integer = 0,i,j;
+    unsigned int integer = 0,i;
+    char *eptr= NULL;
+    unsigned long long convResult = 0U;
     printf("Program name: %s\n", __BASE_FILE__);
-    printf("Enter a non zero positive number: ");
-    scanf("%d", &integer);
-    if(integer < 2)
+    if(argc > 1)
     {
-        printf("The number is %d", integer);
-        return 0;   
-    }
-    for(i=2; i <= integer; i++)
-    {
-        if(checkIsPrime(i))
+        convResult = strtoll(argv[1], (char ** __restrict__ )eptr, 10);
+        if(convResult == 0U)
         {
-            printf(":%d:",i);
-        }
-    }
-    putchar('\n');
+            /* If a conversion error occurred, display a message and exit */
+            if (errno == EINVAL)
+            {
+                printf("Conversion error occurred: %d\n", errno);
+            }
 
+            /* If the value provided was out of range, display a warning message */
+            if (errno == ERANGE)
+                printf("The value provided was out of range\n");
+
+            exit(0);
+        }
+        integer = convResult;
+        if(integer < 2)
+        {
+            printf("The number is %d", integer);
+            return 0;   
+        }
+        for(i=2; i <= integer; i++)
+        {
+            if(checkIsPrime(i))
+            {
+                printf(":%d:",i);
+            }
+        }
+        putchar('\n');
+    }else
+    {
+        printf("Usage...\n");
+    }
     return 0;
 }
