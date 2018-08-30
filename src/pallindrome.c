bd3ad99e8850227c025179686c8ca8d1b608bac7
Sohal Patel <sohal@kodezine.com>
2018-08-30 22:17:40 +0200

    recursive pallindrome

---
+++ src/pallindrome.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+unsigned int stringSize(char* src)
+{
+    char* ptr = src;
+    while(*ptr++);
+    return(ptr-src);
+}
+
+unsigned int strCompare(char *start, char *end)
+{
+    if(*start == *end)
+    {
+        if((start == end) || ((start + 1U) == end))
+        {
+            return( 1 );
+        }
+        return(strCompare(start + 1, end - 1));
+    }else
+    {
+        return( 0 );
+    }
+}
+
+ 
+
+int main(int argc, char *argv[])
+{
+    unsigned int strSize;
+
+    printf("Program name: %s\n", __BASE_FILE__);
+    if(2 == argc)
+    {
+        strSize = stringSize(argv[1]);
+        printf("The string is %s a pallindrome\n",(strCompare(argv[1], argv[1] + strSize - 2U) != 0) ? "\b" : "not");
+    }else
+    {
+        printf("Usage ...\n");
+    }
+    return 0;
+}
--- src/prime.c
+++ src/prime.c
@@ -8,19 +8,11 @@ unsigned int checkIsPrime(unsigned int test)
     for(i=2;i < test ;i++)
     {
         rem = test % i;
-        // printf("%d / %d is %d : %d \n", integer, i, divi, rem);
         if(rem == 0U)
         {
             isPrime = 0;
         }
     }
-    // if(isPrime)
-    // {
-    //     printf("%d is a prime number \n", test);
-    // }else
-    // {
-    //     printf("%d is not a prime number \n", test);
-    // }
     return(isPrime);
 }
 
