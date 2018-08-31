a4b88443f0dd7a259cdcb76c8d84191e1870ebaf
Sohal Patel <sohal@kodezine.com>
2018-08-31 17:08:52 +0200

    Forgot to add these files

---
+++ src/palindrome.h
@@ -0,0 +1,11 @@
+#ifndef _PALINDROME_H_
+#define _PALINDROME_H_
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+uint32_t stringSize(char* src);
+uint32_t strCompare(char *start, char *end);
+
+#endif //_PALINDROME_H_
